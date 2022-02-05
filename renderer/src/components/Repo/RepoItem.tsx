c16267edb339d23da39f305944bc531862a9f309
youthfulhps <ybh942002@gmail.com>
2022-02-06 02:14:00 +0900

    Created RepoItem component

---
+++ renderer/src/components/Repo/RepoItem.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-components';
+
+type RepoItemProps = {
+  repo: any;
+};
+
+const StyledRepoItem = styled.div`
+  background-color: var(--color-100);
+  padding: var(--size-400) 0;
+  color: var(--color-600);
+  border-top: 1px solid var(--color-200);
+  cursor: pointer;
+
+  .repo__title--highlight {
+    font-weight: var(--weight-700);
+    margin-bottom: var(--size-200);
+  }
+
+  .repo__description--blur {
+    color: var(--color-500);
+    font-size: 10px;
+  }
+
+  &:hover {
+    color: var(--color-800);
+  }
+`;
+
+function RepoItem({ repo }: RepoItemProps) {
+  console.log(repo);
+  return (
+    <StyledRepoItem>
+      <div className="repo__title--highlight">{repo.name}</div>
+      <div className="repo__description--blur">{repo.description}</div>
+    </StyledRepoItem>
+  );
+}
+
+export default RepoItem;
