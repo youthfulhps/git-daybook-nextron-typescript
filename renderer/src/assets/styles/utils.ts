2c9b00e5647128e31eb8fc9f4242570fe96c9421
youthfulhps <ybh942002@gmail.com>
2022-03-18 16:52:29 +0900

    Added repo grid template

--- renderer/src/assets/styles/utils.ts
+++ renderer/src/assets/styles/utils.ts
@@ -11,6 +11,15 @@ export const getTemplateAreaBySection = (activeSection: string) => {
           'user div4 div4'
           'div3 div4 div4';
       `;
+    case 'repo':
+      return css`
+        grid-template-areas:
+          'user repo repo'
+          'user repo repo'
+          'div3 repo repo'
+          'div3 repo repo'
+          'div3 div4 div4';
+      `;
     default:
       return css`
         grid-template-areas:
