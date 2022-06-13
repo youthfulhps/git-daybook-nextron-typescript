6fd305b6de126bd39bbc87d4c148c756d3fe51be
youthfulhps <ybh942002@gmail.com>
2022-06-14 01:15:04 +0900

    refactor: modified repo sort by key names

--- renderer/src/apis/repo/types.ts
+++ renderer/src/apis/repo/types.ts
@@ -1,6 +1,6 @@
 export const RepoSortBy = {
-  CREATED: 'created',
-  UPDATED: 'updated',
-  PUSHED: 'pushed',
-  FULL_NAME: 'full_name',
+  created: 'created',
+  updated: 'updated',
+  pushed: 'pushed',
+  fullName: 'full_name',
 } as const;
