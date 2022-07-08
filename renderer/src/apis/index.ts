d0e8604cce48f04f6288a485735a7d7de6a9e818
youthfulhps <ybh942002@gmail.com>
2022-07-09 01:10:54 +0900

    fix: add github token to header to solve issue of exceeding github requests

--- renderer/src/apis/index.ts
+++ renderer/src/apis/index.ts
@@ -1,4 +1,9 @@
 import axios from 'axios';
 export const BASE_URL = 'https://api.github.com';
 
-export const API = axios.create({ baseURL: BASE_URL });
+export const API = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    Authorization: `Token ${process.env.NEXT_PUBLIC_GITHUB_TOKEN}`,
+  },
+});
