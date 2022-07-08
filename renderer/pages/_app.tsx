45f1d9eb45c11b518efca65e59190698d07f6cf3
youthfulhps <ybh942002@gmail.com>
2022-07-09 01:03:45 +0900

    implement: apply user context provider

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -3,13 +3,22 @@ import type { AppProps } from 'next/app';
 import GlobalStyles from '@assets/styles/globalStyles';
 import CommonLayout from '@layouts/CommonLayout';
 import SectionContextProvider from '@contexts/SectionContext';
+import UserContextProvider from '@contexts/UserContext';
 import Head from 'next/head';
 import 'antd/dist/antd.css';
 import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 
 function App({ Component, pageProps }: AppProps) {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchInterval: 1000 * 60 * 15,
+        refetchOnWindowFocus: false,
+        suspense: true,
+      },
+    },
+  });
 
   return (
     <>
@@ -25,11 +34,13 @@ function App({ Component, pageProps }: AppProps) {
       <GlobalStyles />
       <QueryClientProvider client={queryClient}>
         <Hydrate state={pageProps.dehydratedState}>
-          <SectionContextProvider>
-            <CommonLayout>
-              <Component {...pageProps} />
-            </CommonLayout>
-          </SectionContextProvider>
+          <UserContextProvider>
+            <SectionContextProvider>
+              <CommonLayout>
+                <Component {...pageProps} />
+              </CommonLayout>
+            </SectionContextProvider>
+          </UserContextProvider>
         </Hydrate>
         <ReactQueryDevtools />
       </QueryClientProvider>
