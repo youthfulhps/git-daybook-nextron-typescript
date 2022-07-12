a1fdbcfb0deb80684add5572a2f127970b3ab8d6
youthfulhps <ybh942002@gmail.com>
2022-07-13 00:58:45 +0900

    implement: apply contributions hook to user log component

--- renderer/src/components/User/UserLog/index.tsx
+++ renderer/src/components/User/UserLog/index.tsx
@@ -1,7 +1,9 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useMemo } from 'react';
 import styled from 'styled-components';
 import userLanguageList from '@hooks/language/useLanguageList';
 import { UserContext } from '@contexts/UserContext';
+import useContribution from '@hooks/contributions/useContribution';
+import DateUtils from '@utils/DateUtils';
 
 const StyledUserLog = styled.section`
   width: 100%;
@@ -21,12 +23,31 @@ const StyledUserLog = styled.section`
     backdrop-filter: blur(30px);
     background-color: var(--color-800);
   }
+
+  .user-log {
+    &__commit-count {
+      position: absolute;
+      bottom: var(--size-500);
+      right: var(--size-500);
+      color: var(--color-commit-middle);
+      font-size: var(--size-700);
+    }
+  }
 `;
 
 function UserLog() {
   const { userId } = useContext(UserContext);
-  const { totalLanguageList } = userLanguageList(userId);
-  return <StyledUserLog>안녕하세요.</StyledUserLog>;
+
+  const tomorrowDate = DateUtils.getTomorrowDate();
+  const todayDate = DateUtils.getTodayDate();
+  const { contributions } = useContribution(userId, todayDate, tomorrowDate);
+
+  return (
+    <StyledUserLog>
+      오늘은
+      <div className="user-log__commit-count">{contributions.totalCommitContributions}</div>
+    </StyledUserLog>
+  );
 }
 
 export default UserLog;
