9e2aa89b7d399ae6f31ffbb9f2e7182290a5a754
youthfulhps <ybh942002@gmail.com>
2022-07-13 00:55:03 +0900

    update: add contribution params for deadline of total contributions

--- renderer/src/apis/contribution/apis.ts
+++ renderer/src/apis/contribution/apis.ts
@@ -1,9 +1,9 @@
 import { API } from '..';
 import { getContributionsQuery } from '@apis/contribution/graphql';
 
-export const getTodayContributions = (userId: string, since: string) => {
+export const getTodayContributions = (userId: string, from: string, to: string) => {
   const body = {
-    query: getContributionsQuery(userId, since),
+    query: getContributionsQuery(userId, from, to),
   };
 
   return API.post('/graphql', body);
